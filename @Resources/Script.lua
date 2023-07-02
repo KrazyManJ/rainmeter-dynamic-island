165bef28050b0068d9bbe81d0d4bfa2d9f200e24
KrazyManJ <KrazyManJ@gmail.com>
2023-07-02 14:24:31 +0200

    Added Config file, added full alpha color and stroke to island, updated script with function of toggling values in config

--- @Resources/Script.lua
+++ @Resources/Script.lua
@@ -1,3 +1,13 @@
 function Map(value, fMin, fMax, tMin, tMax)
     return (value - fMin) * (tMax - tMin) / (fMax - fMin) + tMin;
 end
+
+function Ternary(cond, one, elseVal)
+    if cond then return elseVal else return one end
+end
+
+function ToggleConfigValue(varName, varVal, firstVal, secondVal)
+    local var = Ternary(varVal == firstVal, firstVal, secondVal)
+    SKIN:Bang("!WriteKeyValue", "Variables", varName, var, "#@#Config.inc")
+    SKIN:Bang("!SetVariable", varName, var)
+end
