d010c2220ba30ac2933dbed0ca462ec34f7992b6
KrazyManJ <KrazyManJ@gmail.com>
2023-07-02 16:54:15 +0200

    Updated Music Widget Animations, removed hover color change because it was buggy, updated script to handle context index

--- multi_monitor_copy.py
+++ multi_monitor_copy.py
@@ -20,6 +20,6 @@ for i in range(COPY_NUMBERS):
         dest_path = os.path.join(monitor_dir, filename)
         
         lines = open(src_path,"r").readlines()
-        lines.insert(8,f"    ContextAction5=[!Move (#SCREENAREAX@{i+2}#+#SCREENAREAWIDTH@{i+2}#/2-#C_SKIN_WIDTH#/2-#C_SKIN_MARGIN#) 0]\n")
+        lines.insert(8,f"    ContextAction{len(files_to_copy)+2}=[!Move (#SCREENAREAX@{i+2}#+#SCREENAREAWIDTH@{i+2}#/2-#C_SKIN_WIDTH#/2-#C_SKIN_MARGIN#) 0]\n")
         open(dest_path,"w").write("".join(lines))
         
